74b51de542b16442ab0b1c4ac1394d7b1d584b0c
Marcus Pernow <marcus.pernow@gmail.com>
2024-07-21 18:03:21 +0200

    examples 2.4

---
+++ 2-review/4-local-vars/address.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void func(int *y)
+{
+    cout << "Address of y is " << y << endl;
+    *y = 1;
+}
+
+int main()
+{
+    int x = 2;
+    cout << "Address of x is " << &x << endl;
+    func(&x);
+    cout << "After calling func(), x is " << x << endl;
+}
---
+++ 2-review/4-local-vars/value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int y)
+{
+    cout << "Address of y is " << &y << endl;
+    return y;
+}
+
+int main()
+{
+    int x = 2;
+    cout << "Address of x is " << &x << endl;
+    int z = func(x);
+    cout << "Address of z is " << &z << endl;
+    cout << "After calling func(), z is " << z << endl;
+}
