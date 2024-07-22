40653d9b7b12a185cf67465ddb1eea800142ffb8
Marcus Pernow <marcus.pernow@gmail.com>
2024-07-22 14:51:11 +0200

    assignment 1

--- 2-review/assignment1/test_url.cpp
+++ 2-review/assignment1/test_url.cpp
@@ -1,10 +1,10 @@
 // test_url.cpp
-// compile with: g++ -o test_url url.cpp test_url.cpp && ./test_url
+// compile with: g++ -o test_url url.cpp test_url.cpp
 #include <iostream>
 #include "url.h"
 
 int main()
 {
-    Url url("http", "www.example.com/index.html");
+    Url url{"http", "www.example.com/index.html"};
     url.printUrl();
 }
