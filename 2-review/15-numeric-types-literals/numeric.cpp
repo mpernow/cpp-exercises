8fbe7df252ad15601eb27f83c0a83c40bb815637
Marcus Pernow <marcus.pernow@gmail.com>
2024-07-23 11:12:23 +0200

    exercises 2.15

---
+++ 2-review/15-numeric-types-literals/numeric.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int one_million = 1'000'000; // Use the ' separator for readability
+    const int one_lakh = 1'00'000;
+    double pi = 3.141'593;
+
+    cout << "One million: " << one_million << endl;
+    cout << "One lakh: " << one_lakh << endl;
+    cout << "pi: " << pi << endl;
+}
