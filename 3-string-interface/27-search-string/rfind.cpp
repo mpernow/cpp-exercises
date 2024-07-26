005eb4533fbac96b9db359e6fd9f9bbe3f160693
Marcus Pernow <marcus.pernow@gmail.com>
2024-07-26 10:01:56 +0200

    exercise 3.27

---
+++ 3-string-interface/27-search-string/rfind.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str{"Hello world"};
+    cout << "Initial contents of str: " << str << endl;
+
+    size_t pos = str.rfind('o');
+
+    if (pos != string::npos)
+    {
+        str[pos] = 'p';
+    }
+    else
+    {
+        cout << "Could not find the search string\n";
+    }
+
+    cout << "Final contents of str:   " << str << endl;
+}
