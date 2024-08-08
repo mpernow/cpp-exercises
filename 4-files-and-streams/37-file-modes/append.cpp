a4409a69ad51466448bc1893f977096a5d038fd1
Marcus Pernow <marcus.pernow@gmail.com>
2024-08-08 20:50:07 +0200

    exercises 4.37

---
+++ 4-files-and-streams/37-file-modes/append.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ofstream ofile;
+    ofile.open("important.txt", fstream::app);
+
+    if (!ofile)
+    {
+        cout << "Could not open file" << endl;
+        return -1;
+    }
+
+    ofile << "some more data";
+    ofile.close();
+}
