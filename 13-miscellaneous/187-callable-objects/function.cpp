92870a2f06a8b0e4f7618913dc73a6d7a52f5ad6
Marcus Pernow <marcus.pernow@gmail.com>
2025-05-14 21:39:58 +0200

    exercises 13.187

---
+++ 13-miscellaneous/187-callable-objects/function.cpp
@@ -0,0 +1,69 @@
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace placeholders;
+
+// Non-member function
+bool match(const string &test) { return test == "cat"; }
+
+// Functor
+class functor_match
+{
+  public:
+    bool operator()(const string &test) { return test == "cat"; }
+};
+
+bool bind_match(const string &animal, const string &species)
+{
+    return animal == species;
+}
+
+int count_strings(vector<string> &texts,
+                  function<bool(const string &)> match_ptr)
+{
+    int tally = 0;
+    for (auto text : texts)
+    {
+        if (match_ptr(text))
+        {
+            ++tally;
+        }
+    }
+    return tally;
+}
+
+int main()
+{
+    vector<string> animals = {"cat",  "dog", "tiger",  "lion",
+                              "bear", "cat", "giraffe"};
+    int n;
+
+    // Function pointer
+    cout << "Calling count_strings with function pointer" << endl;
+    n = count_strings(animals, match);
+    cout << R"(The vector contains )" << n
+         << R"( occurrences of the word "cat")" << endl;
+
+    // Functor
+    cout << "\nCalling count_strings with functor" << endl;
+    n = count_strings(animals, functor_match());
+    cout << R"(The vector contains )" << n
+         << R"( occurrences of the word "cat")" << endl;
+
+    // Lambda expression
+    cout << "\nCalling count_strings with lambda expression\n";
+    n = count_strings(animals,
+                      [](const string &test) { return test == "cat"; });
+    cout << R"(The vector contains )" << n
+         << R"( occurrences of the word "cat")" << endl;
+
+    // bind() object
+    cout << "\nCalling count_strings with bind() object\n";
+    auto match_cat = bind(bind_match, _1, "cat");
+    n = count_strings(animals, match_cat);
+    cout << R"(The vector contains )" << n
+         << R"( occurrences of the word "cat")" << endl;
+}
